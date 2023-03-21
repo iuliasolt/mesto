f2f1e8827fe0184572e3f3c12b7e9def13731f8a
Julia Soltyk <iulia1502@yandex.ru>
2023-03-21 16:39:12 +0300

    Исправления после первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,27 @@
-const edit = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup_type_more-info");
-const close = popup.querySelector(".popup__close");
-const nameEdit = popup.querySelector(".popup__name");
-const jobEdit = popup.querySelector(".popup__job");
-const changeName = document.querySelector(".profile__name");
-const changeJob = document.querySelector(".profile__job");
-const save = popup.querySelector(".popup__save");
-const form = popup.querySelector(".popup__form");
+const edit = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup_type_more-info');
+const close = popup.querySelector('.popup__close');
+const nameEdit = popup.querySelector('.popup__input_type_name');
+const jobEdit = popup.querySelector('.popup__input_type_job');
+const changeName = document.querySelector('.profile__name');
+const changeJob = document.querySelector('.profile__job');
+const form = popup.querySelector('.popup__form');
 
-edit.addEventListener("click", () => {
-    console.log("click");
-
-popup.classList.add("popup_opened")
+edit.addEventListener('click', () => {
+    popup.classList.add('popup_opened'); /*Открытие попапа по клику кнопки edit-button*/
+    nameEdit.value = changeName.textContent; /*Предзаполненяем значение input в форме. Сохраняет значение в попапе при открытии.*/
+    jobEdit.value = changeJob.textContent;
 });
 
-close.addEventListener("click", () => {
-    popup.classList.remove("popup_opened")
+close.addEventListener('click', () => {
+    popup.classList.remove("popup_opened"); /*Закрытие попапа по клику на кнопку close*/
 });
 
+/*Обработчик submit*/
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); /*event - чтобы страница не перезагружалась*/
 
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const name = nameEdit.value;
-    const job = jobEdit.value;
-
-    changeName.textContent = name;
-    changeJob.textContent = job;
+    changeName.textContent = nameEdit.value;
+    changeJob.textContent = jobEdit.value;
+    popup.classList.remove('popup_opened'); /*Закрытие попапа после внесенных изменений*/
 });
